1ed63ee679ac5cd84808ab4c08835949242380dd
dgfd-sgiad <ekouhontode@finances.bj>
2026-08-06 18:26:37 +0100

    Fix: distinct IDs and images for each module

--- js/config.js
+++ js/config.js
@@ -18,7 +18,7 @@ const modules = [
         file: 'modules/coop_dec.html'
     },
     {
-        id: 'suivi_financier_projets',
+        id: 'suivi_des_projets',
         title: 'Suivi Financier des Projets',
         icon: '🎓',
         file: 'modules/suivi.html'
